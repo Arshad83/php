13d8ce987c8c56f1918f2a4ed90666d8f2d6efed
syedarshadali <arshad@3esofttech.com>
2018-02-09 16:35:57 +0530

    learning from Moodle coding to enhance coding

--- phpoops/php_extract.php
+++ phpoops/php_extract.php
@@ -1,20 +1,40 @@
 <?php
 abstract class calculation{
 	public $value;
-	public function getCalculation($value){
+	public $name;
+	public function __construct($value,$name){
 		$this->value=$value;
+		$this->name=$name;
+
+		
 	}
+
+	
 }
 class Measure extends calculation{
-	public $value=20;
-	public $name="server";
-	public $meaure="measure";
-}
+	
+	public $meaure;
+	public function __construct($value,$string1,$string2){
+		parent::__construct($value,$string2);
+		$this->measure=$string1;
+	}
+	// public static function getCalculate($value,$string1){
+	// 	parent::__construct($value,$string1);
+	// }
 
-class Dimension{
-	public function getValue(calculation $measure){
-		echo $measure->value;
+}
+class DD{
+	public static function validate(calculation $measure){
+		echo $measure->name;
+	}
+	public static function getParam(){
+		return new Measure(20,'server','arshad');
+	}
+}
+class Dimension extends DD{
+	public function getValue(){
+		self::validate(self::getParam());
 	}
 }
 $obj=new Dimension;
-$obj->getValue(new Measure());
+$obj->getValue();
